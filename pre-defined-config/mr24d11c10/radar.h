39b2e4c67a2f9c975d3018332b9938d56e85c978
TheRealTom <neportegal@seznam.cz>
2023-05-09 17:47:54 +0200

    Working predefined module

--- pre-defined-config/mr24d11c10/radar.h
+++ pre-defined-config/mr24d11c10/radar.h
@@ -4,8 +4,12 @@
  * Accessed: 5/7/2023
  * License: MIT
  */
+namespace esphome {
+namespace radar_ns {
+
 #ifndef _RADAR_H__
 #define _RADAR_H__
+
 
 class radar
 {
@@ -15,8 +19,10 @@ class radar
         int Situation_judgment(int ad1, int ad2, int ad3, int ad4, int ad5);
         int Fall_judgment(int ad1, int ad2, int ad3, int ad4);
         char CRC(char ad1, char ad2, char ad3, char ad4, char ad5, char ad6, char ad7);
-        static unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len);
+        unsigned short int us_CalculateCrc16(unsigned char *lpuc_Frame, unsigned short int lus_Len);
 };
-
 #endif
+
+}  // namespace radar
+}  // namespace esphome
 /** END of copied and changed content */
